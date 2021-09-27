956ce2563b645b8c304d44b4d9f76a9a93d3a27a
AbdulRehman787 <88674059+AbdulRehman787@users.noreply.github.com>
2021-09-27 10:31:27 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+// var arr = [
+//     ["ahmed", 0]
+//     ["Raza",1]
+// ]
+
+
+// var a = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+// ];
+// for (var i = 0; i < 3; i++) {
+//     for (let j = 0; j < 4; j++) {
+//         document.write(a[i][j]+" ");
+//     }
+//      document.write("</br>");
+// }
+
+
+// for(var i=1;i<=10;i++){
+//     document.write(i)
+// }
+
+// var x = prompt("Enter the number you want table");
+// var y = prompt("enter the length of table")
+// for(var i=0;i<=y;i++){
+//     document.write(x+ "* "+ i + " = "+x*i +"<br/>");
+// }
+
+// var fruits = ["apple", "banana", "mango", "orange","strawberry"]
+// console.log(fruits)
+
+// var counting = [0,1,2,3,4,5,6,7,8,9,10]
+// var reverseCount=[10,9,8,7,6,5,4,3,2,1,0]
+// var evenNum = [2,4,6,8,10,12, 14, 16, 18, 20]
+// var oddNum = [1,3,5,7,9,11,13,15,17,19]
+// var series = ["2k", "4k", "6k", "8k", "10k", "12k", "14k", "16k", "18k", "20k"]
+// document.write("counting" + counting   +"<br/>" + " reverseCount" +reverseCount +"<br/>"+ "evenNum" +evenNum +"<br/>"+"oddNum"+ oddNum+"<br/>" + "series" +series)
+
+var a = prompt("Enter the your order")
+var b = ["cake", " apple" ,"pie" , "cookie", "chips", "patties"]
+if(b>=a){
+    alert("your order is complete")
+} else{
+    alert("Your order is not complete")
+}
